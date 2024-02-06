b47061b6a22f3747eadb188b845aea82a2780c97
wanetty <eduard.gm@gmail.com>
2024-02-06 17:59:47 +0100

    initial commit

---
+++ src/pages/blog/test.md
@@ -0,0 +1,7 @@
+## Esto es una MD de prueba
+
+Este texto es de prueba y es un md
+
+```bash
+rm -rf
+```
---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+import mod from 'astro/zod'
+import { ModuleDetectionKind } from 'typescript'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
