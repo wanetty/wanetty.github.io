2a2672fda6b616c34210af16c861ca2fdd07b8fd
wanetty <eduard.gm@gmail.com>
2024-05-05 20:42:40 +0200

    Cloudflare estado

--- src/functions/getTitle.ts
+++
@@ -1,7 +0,0 @@
-export async function getServerData(URL: string): Promise<Response> {
-    const response = await fetch(URL);
-    const data = await response.json();
-    return new Response(JSON.stringify({ mensaje: data.title }), {
-        headers: { 'Content-Type': 'application/json' }
-    });
-}
