a34c24d418ffb1db0d74d93296f62e3de0d83d60
wanetty <eduard.gm@gmail.com>
2024-09-04 17:47:42 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from "@astrojs/cloudflare";
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   site: 'https://blog.wanetty.com',
   output: "server",
   adapter: cloudflare({
